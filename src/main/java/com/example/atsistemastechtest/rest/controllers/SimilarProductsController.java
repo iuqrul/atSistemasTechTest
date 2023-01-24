4ba549f57cf56a128edc068fcf80fb172fa515b6
iuqrul <iuqrul@gmail.com>
2023-01-24 10:14:16 +0100

    Expose OpenAPI definition

--- src/main/java/com/example/atsistemastechtest/rest/controllers/SimilarProductsController.java
+++ src/main/java/com/example/atsistemastechtest/rest/controllers/SimilarProductsController.java
@@ -4,11 +4,11 @@ import com.example.atsistemastechtest.openapitools.client.model.ProductDetail;
 import com.example.atsistemastechtest.services.SimilarProducts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class SimilarProductsController {
 
     @Autowired
